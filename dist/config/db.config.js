da6cf41b2d6d13fc6c46779c972ad65e673917c2
Hill-Man-1 <hilmaaansf@gmail.com>
2023-08-19 01:43:50 +0700

    pls

--- dist/config/db.config.js
+++ dist/config/db.config.js
@@ -8,9 +8,16 @@ exports.DBConfig = {
     DB_NAME: process.env.DB_NAME
 };
 exports.DB_RailwayConfig = {
-    HOST: process.env.DB_RAILWAY_HOST,
-    PORT: process.env.DB_RAILWAY_PORT,
-    USER: process.env.DB_RAILWAY_USERNAME,
-    PASSWORD: process.env.DB_RAILWAY_PASSWORD,
-    DB_NAME: process.env.DB_RAILWAY_NAME
+    HOST: process.env.DB_RAILWAY_HOST || 'localhostcontainers-us-west-42.railway.app',
+    USER: process.env.DB_RAILWAY_USER || 'root',
+    PASSWORD: process.env.DB_RAILWAY_PASSWORD || '8SmqdBhCH4ymAgc4evs7',
+    PORT: process.env.DB_RAILWAY_PORT ? parseInt(process.env.DB_RAILWAY_PORT) : 3306,
+    DB_RAILWAY_NAME: process.env.DB_RAILWAY_NAME || 'railway'
 };
+// export const redisConnection =  {
+//     HOST : process.env.REDIS_HOST,
+//     URL : process.env.REDIS_URL,
+//     PASSWORD : process.env.REDIS_PASSWORD,
+//     PORT : process.env.REDIS_PORT,
+//     USER : process.env.REDIS_USER,
+// }
