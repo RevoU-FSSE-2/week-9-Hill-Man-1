65110e7c7644fa4974871a39722bb684a3daea18
Hill-Man-1 <hilmaaansf@gmail.com>
2023-08-19 01:13:03 +0700

    try to fix connection

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,59 +1,50 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from 'express';
+import Redis from 'ioredis';
 import { db } from "../config/db.connection";
-import Redis from "ioredis";
-import { RowDataPacket } from "mysql2";
-
-interface UserInfo {
-    id: number;
-    name: string;
-    address: string;
-    balance: number; // Add the balance property here
-    // total_income: number;
-    total_expense: number;
-}
+import { RowDataPacket } from 'mysql2';
 
 export const getUserInfo = async (req: Request, res: Response) => {
     const userId = req.params.id;
+
+    const redisKey = `user:${userId}`; 
     const redisClient = new Redis();
 
-    try {
-        const cachedData = await redisClient.get(userId);
+
+    redisClient.get(redisKey, async (redisErr, cachedData) => {
         if (cachedData) {
             res.json(JSON.parse(cachedData));
             console.log('get data from cache');
         } else {
-            const sql = `
-                SELECT users.id, name, address,
-                SUM(CASE WHEN type = 'income' THEN amount ELSE 0 END) AS total_income,
-                SUM(CASE WHEN type = 'expense' THEN amount ELSE 0 END) AS total_expense
-                FROM users
-                LEFT JOIN transactions ON users.id = transactions.user_id 
-                WHERE users.id = ?
-                GROUP BY users.id`;
-            
-            const [result] = await db.query<RowDataPacket[]>(sql, [userId]);
+            const sql = 'SELECT users.id, name, address, ' +
+                'SUM(CASE WHEN type="income" THEN amount ELSE 0 END) AS total_income, ' +
+                'SUM(CASE WHEN type="expense" THEN amount ELSE 0 END) AS total_expense ' +
+                'FROM users ' +
+                'LEFT JOIN transactions ON users.id = transactions.user_id ' +
+                'WHERE users.id = ? ' +
+                'GROUP BY users.id';
 
-            if (result && result.length > 0) {
-                const userData: UserInfo = {
-                    id: result[0].id,
-                    name: result[0].name,
-                    address: result[0].address,
-                    // total_income: result[0].total_income,
-                    balance: result[0].total_income - result[0].total_expense,
-                    total_expense: result[0].total_expense
-                };
+            try {
+                const [result] = await db.query<RowDataPacket[]>(sql, [userId]);
+                if (result.length > 0) {
+                    const userData = {
+                        id: result[0].id,
+                        name: result[0].name,
+                        address: result[0].address,
+                        balance: result[0].total_income - result[0].total_expense,
+                        expense: result[0].total_expense
+                    };
 
-                await redisClient.setex(userId, 3600, JSON.stringify(userData));
-                res.json(userData);
-                console.log('add new data to cache');
-            } else {
-                res.status(404).json({ error: 'User not found' });
+                    // Set data in Redis cache
+                    redisClient.setex(redisKey, 60, JSON.stringify(userData)); // Cache for 1 hour
+                    res.json(userData);
+                    console.log('add new data to cache');
+                } else {
+                    res.status(404).json({ error: 'User not found' });
+                }
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({ error: 'An error occurred while fetching user information' });
             }
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while fetching user information' });
-    } finally {
-        redisClient.disconnect();
-    }
+    });
 };
